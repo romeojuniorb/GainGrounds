1cbfbd11e630d0bd5449bab7bb1115c2c229c3da
romeojuniorb <163235833+romeojuniorb@users.noreply.github.com>
2024-03-14 03:32:48 -0400

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,15 +1,15 @@
-const Joi = require('joi');
+// const Joi = require('joi');
 
-module.exports.lifts = Joi.object({
-    userId: Joi.string().required(),
-    date: Joi.date().required(),
-    bench: Joi.object({
-        weightLifted: Joi.number().min(0).precision(2)
-    }),
-    squat: Joi.object({
-        weightLifted: Joi.number().min(0).precision(2)
-    }),
-    deadlift: Joi.object({
-        weightLifted: Joi.number().min(0).precision(2)
-    })
-}).or('bench', 'squat', 'deadlift'); 
+// module.exports.lifts = Joi.object({
+//     userId: Joi.string().required(),
+//     date: Joi.date().required(),
+//     bench: Joi.object({
+//         weightLifted: Joi.number().min(0).precision(2)
+//     }),
+//     squat: Joi.object({
+//         weightLifted: Joi.number().min(0).precision(2)
+//     }),
+//     deadlift: Joi.object({
+//         weightLifted: Joi.number().min(0).precision(2)
+//     })
+// }).or('bench', 'squat', 'deadlift'); 
