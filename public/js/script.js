1752002203f148b6d6d32729f3daa0a0862168bb
Romeo Barbieto Junior <163235833+romeojuniorb@users.noreply.github.com>
2024-03-17 21:20:46 -0400

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,18 @@
-const menuShowBtn = document.getElementById('menu-show-btn');
-const menuHideBtn = document.getElementById('menu-hide-btn');
-const menuOverlay = document.querySelector('.nav-overlay');
+document.addEventListener("DOMContentLoaded", function () {
+  var menuShowBtn = document.getElementById("menu-show-btn");
+  var menuHideBtn = document.getElementById("menu-hide-btn");
+  var navOverlay = document.querySelector(".nav-overlay");
 
-menuShowBtn.addEventListener('click', () => menuOverlay.classList.add('show-overlay'));
-menuHideBtn.addEventListener('click', () => menuOverlay.classList.remove('show-overlay'));
+  menuShowBtn.addEventListener("click", function () {
+    navOverlay.classList.add("show-overlay");
+  });
+
+  menuHideBtn.addEventListener("click", function () {
+    navOverlay.classList.remove("show-overlay");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const today = new Date().toISOString().split("T")[0];
+  document.getElementById("date").value = today;
+});
